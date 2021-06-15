a189893505afefc66ef90b6ffe251ed9ef8260c7
Dmitry Budko <budkodmv@gmail.com>
2021-06-15 13:21:47 +0400

    add swagger annotations

--- app/Http/Resources/BookCollection.php
+++ app/Http/Resources/BookCollection.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
+/**
+ * @OA\Schema(
+ *     @OA\Xml(name="BookCollection"),
+ *     @OA\Property(
+ *         property="books",
+ *         description="book list",
+ *         type="array",
+ *         @OA\Items(ref="#/components/schemas/Book")
+ *     )
+ * )
+ */
 class BookCollection extends ResourceCollection
 {
+    public static $wrap = 'books';
     /**
      * Transform the resource collection into an array.
      *
@@ -14,6 +27,10 @@ class BookCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'code' => 200,
+            'books' => $this->collection,
+            'timestamp' => Carbon::now(),
+        ];
     }
 }
