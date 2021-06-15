058a9e892fa0d0a3c444eee26d06fc1af8b1a2dd
Dmitry Budko <budkodmv@gmail.com>
2021-06-15 13:16:33 +0400

    add migaration many-to-many authors-to-books

---
+++ database/migrations/2021_06_08_073449_create_author_book_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author_book', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('book_id');
+            $table->timestamps();
+
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('authors')
+                ->cascadeOnDelete();
+            $table->foreign('book_id')
+                ->references('id')
+                ->on('books')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author_book');
+    }
+}
